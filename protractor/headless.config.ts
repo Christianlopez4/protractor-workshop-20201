fd428290cb9fe2bd1dbb2e356e5ac844791d8fda
Christian Felipe Lopez Cleves <38162475+ChristianCleves@users.noreply.github.com>
2020-07-27 15:57:10 -0500

    css selector

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -1,18 +1,19 @@
-import { Config, browser } from 'protractor';
-import { reporter } from './helpers/reporter';
+import { Config, browser } from "protractor";
+import { reporter } from "./helpers/reporter";
 
 export const config: Config = {
-  framework: 'jasmine',
-  specs: ['../test/google.spec.js'],
+  framework: "jasmine",
+  specs: ["../test/**/*.spec.js"],
+  getPageTimeout: 1000,
   SELENIUM_PROMISE_MANAGER: false,
   onPrepare: () => {
     browser.ignoreSynchronization = true;
     reporter();
   },
   capabilities: {
-    browserName: 'chrome',
+    browserName: "chrome",
     chromeOptions: {
-      args: ['--headless', '--disable-gpu'],
+      args: ["--headless", "--disable-gpu"],
     },
   },
 };
